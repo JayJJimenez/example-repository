3708b156be74082b25a8c5d95ac4550ae57b444a
JonathanJimenez <jayjjimenez@gmail.com>
2021-03-26 18:45:43 -0400

    add images to style.css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let h2 = document.createElement('h2')
-h2.textContent = "This content added by JavaScript"
+h2.textContent = "Founded by Jonathan Jimenez in 2021. We are dedicated to bringing you the best cofee beverages in NYC!"
 
 document.querySelector('body').appendChild(h2)
